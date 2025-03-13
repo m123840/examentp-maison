49a7cce47f2872d86ca9e2d9fba6af6db110bab8
mariem abida <mariemabida30@gmail.com>
2025-03-13 21:26:21 +0100

    Correction de project.py avec vérification si la liste est vide

--- project.py
+++ project.py
@@ -1,1 +1,7 @@
-Data=[1,3,5)\nprint ('la somme est :', sum(Data))\nprint ('le min est :', min(Data))\nprint ('le max est :', max(Data))
+Data=[1,3,5]
+if Data:
+    print ('la somme est :', sum(Data))
+    print ('le min est :', min(Data))
+    print ('le max est :', max(Data))
+else:
+    print ('dossier vide')
